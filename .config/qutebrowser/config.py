149d68548d5a657d156833fdd984e4711c90b405
Kevin Le <kebinhleh@gmail.com>
2025-07-05 00:16:19 -0500

    qute

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,7 @@
+c.tabs.position = 'left'
+c.completion.shrink = True
+
+config.source('themes/qute-city-lights/city-lights-theme.py')
+config.set("colors.webpage.darkmode.enabled", True)
+
+config.load_autoconfig(False)
